99cc6717a14e6db387ba6e42ae00ccae0ef970c3
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 14:50:58 +0200

    python312Packages.eth-typing: 4.0.0 -> 5.0.0

--- pkgs/development/python-modules/eth-typing/default.nix
+++ pkgs/development/python-modules/eth-typing/default.nix
@@ -1,9 +1,10 @@
 {
   lib,
-  fetchFromGitHub,
   buildPythonPackage,
+  fetchFromGitHub,
   pythonOlder,
   pytestCheckHook,
+  pytest-xdist,
   typing-extensions,
   setuptools,
 }:
@@ -13,7 +14,7 @@ buildPythonPackage rec {
   version = "5.0.1";
   pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
@@ -24,17 +25,20 @@ buildPythonPackage rec {
 
   build-system = [ setuptools ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
-
   dependencies = [ typing-extensions ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-xdist
+  ];
 
   pythonImportsCheck = [ "eth_typing" ];
 
-  meta = with lib; {
+  meta = {
+    changelog = "https://github.com/ethereum/eth-typing/blob/v${version}/docs/release_notes.rst";
     description = "Common type annotations for Ethereum Python packages";
     homepage = "https://github.com/ethereum/eth-typing";
-    changelog = "https://github.com/ethereum/eth-typing/blob/v${version}/docs/release_notes.rst";
-    license = licenses.mit;
-    maintainers = with maintainers; [ siraben ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ siraben ];
   };
 }
